a6d66627f2098e7f263b769141246e21f3fa1daa
myystxx <xng034@e.ntu.edu.sg>
2021-04-06 10:42:19 +0800

    some changes on loading & topics

--- lookingglass/src/pages/loading.js
+++ lookingglass/src/pages/loading.js
@@ -11,14 +11,14 @@ export default function Loading() {
     return (
         // <div id="loading_page" style={{ position: "relative", height: "100%"}}>
         <div id="loading_div" className="success">
-            <div style={{ position: "absolute" }}>
-                <img src={lg_logo} style={{ width: '20%', marginTop: "70%" }} />
+            <div style={{ position: "relative" }}>
+                <img src={lg_logo} style={{ width: '20%', marginTop: "60%" }} />
                 <div style={{ marginTop: "2%", textAlign: "center", fontSize: "12px" }}>by <span style={{ color: "rgb(161,64,72)" }}> <img
                     src={cn_logo}
                     style={{
                         maxWidth: "20%"
                     }} /></span></div>
-                <div style={{marginTop: "80%", textAlign: "center", textSizeAdjust: "auto" }}> Powered by </div>
+                <div style={{marginTop: "70%", textAlign: "center", fontSize: "18px" }}> Powered by </div>
                 <img src={nml_log} style={{marginBottom: "10%",  width: '30%'}} />
             </div>
         </div>
